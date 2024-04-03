d8e2e54c3da13ab9b07fbdfc273db1265690cbc2
Nourbj <nbenjannena@gmail.com>
2024-04-03 20:59:31 +0100

    commit

--- server/users/models.py
+++ server/users/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, first_name, last_name, password=None, **extra_fields):
+    def create_user(self, email, username, password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, first_name=first_name, last_name=last_name, **extra_fields)
+        user = self.model(email=email, username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, first_name, last_name, password=None, **extra_fields):
+    def create_superuser(self, email, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -21,7 +20,8 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, username, first_name, last_name, password, **extra_fields)
+        return self.create_user(email, username, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=100)
    # address = models.ForeignKey('Address', on_delete=models.CASCADE, null=True, related_name='addresses')
