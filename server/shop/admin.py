b48577014ab640bdc4b0afcfba20bea255d3163f
hadil <hadilyahiaoui02@gmail.com>
2024-04-03 06:14:23 +0100

    re-setting server

--- server/shop/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Shop
-
-# Register your models here.
-class ShopAdmin(admin.ModelAdmin):
-    list_display=('name',)
-admin.site.register(Shop,ShopAdmin)
