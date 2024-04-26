f94a9b4a401d69f3620d9b9c116e48a7062f7d7f
nadazcx <zcnadaa@gmail.com>
2024-04-26 11:51:19 +0100

    fixing padding and centering signup and login form

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   },
   {
     path: 'signup',
-    loadChildren: () => import('./pages/user/signup/signup.module').then( m => m.SignupPageModule)
+    loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
   },
   {
     path: 'login',
@@ -34,15 +34,15 @@ const routes: Routes = [
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/user/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   },
   {
     path: 'settings',
-    loadChildren: () => import('./pages/user/settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
   },
   {
     path: 'edit-profile',
-    loadChildren: () => import('./pages/user/edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
+    loadChildren: () => import('./pages/edit-profile/edit-profile.module').then( m => m.EditProfilePageModule)
   },
   {
     path: 'home/p1',
@@ -66,7 +66,7 @@ const routes: Routes = [
   }, 
   { 
     path: 'all-items/item-details/:item_id', 
-    loadChildren: () => import('./pages/user/item-details/item-details.module').then(m => m.ItemDetailsPageModule)
+    loadChildren: () => import('./pages/item-details/item-details.module').then(m => m.ItemDetailsPageModule)
   },
   {
     path: 'cart',
@@ -74,15 +74,15 @@ const routes: Routes = [
   },
   {
     path: 'likes',
-    loadChildren: () => import('./pages/user/likes/likes.module').then( m => m.LikesPageModule)
+    loadChildren: () => import('./pages/likes/likes.module').then( m => m.LikesPageModule)
   },
   {
     path: 'profile',
-    loadChildren: () => import('./pages/user/profile/profile.module').then( m => m.ProfilePageModule)
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
   },
   {
     path: 'store',
-    loadChildren: () => import('./pages/user/store/store.module').then( m => m.StorePageModule)
+    loadChildren: () => import('./pages/store/store.module').then( m => m.StorePageModule)
   },
   {
     path: 'signup-shop',
@@ -106,27 +106,27 @@ const routes: Routes = [
   
   {
     path: 'search-articles',
-    loadChildren: () => import('./pages/user/search-articles/search-articles.module').then( m => m.SearchArticlesPageModule)
+    loadChildren: () => import('./pages/search-articles/search-articles.module').then( m => m.SearchArticlesPageModule)
   },
   {
     path: 'search-bezbezes',
-    loadChildren: () => import('./pages/user/search-bezbezes/search-bezbezes.module').then( m => m.SearchBezbezesPageModule)
+    loadChildren: () => import('./pages/search-bezbezes/search-bezbezes.module').then( m => m.SearchBezbezesPageModule)
   },
   {
     path: 'payment-success',
-    loadChildren: () => import('./pages/user/payment-success/payment-success.module').then( m => m.PaymentSuccessPageModule)
+    loadChildren: () => import('./pages/payment-success/payment-success.module').then( m => m.PaymentSuccessPageModule)
   },
   {
     path: 'payment',
-    loadChildren: () => import('./pages/user/payment/payment.module').then( m => m.PaymentPageModule)
+    loadChildren: () => import('./pages/payment/payment.module').then( m => m.PaymentPageModule)
   },
   {
     path: 'payment-method',
-    loadChildren: () => import('./pages/user/payment-method/payment-method.module').then( m => m.PaymentMethodPageModule)
+    loadChildren: () => import('./pages/payment-method/payment-method.module').then( m => m.PaymentMethodPageModule)
   },
   {
     path: 'add-card',
-    loadChildren: () => import('./pages/user/add-card/add-card.module').then( m => m.AddCardPageModule)
+    loadChildren: () => import('./pages/add-card/add-card.module').then( m => m.AddCardPageModule)
   },
  
 
